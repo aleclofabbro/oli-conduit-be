a66e7aede595efa23beb4136cbf5472444166c01
aleclofabbro <agiansanti@uniquid.com>
2018-01-11 02:25:48 +0100

    Banned Error ... Error is a Value .. .

--- src/network/articleNode.ts
+++ src/network/articleNode.ts
@@ -1,17 +1,15 @@
 import { Observable } from '@reactivex/rxjs/dist/package/Rx';
 import axios from 'axios';
-import { build, RequestHandler } from '../types/lib/serviceNode';
+import { build, RequestHandler, RequestStatus } from '../types/lib/serviceNode';
 import {
   ArticleRequest,
-  ArticleValue,
-  ArticleError
+  ArticleValue
 } from '../types/services/Article';
-type ArticleRequestHandler = RequestHandler<ArticleRequest, ArticleValue, ArticleError>;
+type ArticleRequestHandler = RequestHandler<ArticleRequest, ArticleValue>;
 const baseUrl = 'https://conduit.productionready.io/api';
 export const articleRequestHandler: ArticleRequestHandler =
-  (slug, val, error, end) => Observable.fromPromise(axios.get<ArticleValue>(`${baseUrl}/articles/${slug}`))
-    .map(resp => resp.data)
-    .subscribe(val, error, end);
+  (slug) => Observable.fromPromise(axios.get<ArticleValue>(`${baseUrl}/articles/${slug}`))
+    .map(resp => resp.data);
 
-export const articleNode$ = build<ArticleRequest, ArticleValue, ArticleError>('articles', articleRequestHandler);
+export const articleNode$ = build<ArticleRequest, ArticleValue>('articles', articleRequestHandler);
 export default articleNode$;
--- src/types/lib/serviceNode.ts
+++ src/types/lib/serviceNode.ts
@@ -1,120 +1,89 @@
 import { Subject } from '@reactivex/rxjs/dist/package/Rx';
 import { Observable } from '@reactivex/rxjs/dist/package/Observable';
-export type RequestHandler<Request, Value, Error> = {
+export type RequestHandler<Request, Value> = {
   (
     request: Request,
-    value: { (val: Value): void; },
-    error: { (error: Error): void; },
-    end: { (): void; }
-  ): void;
+  ): Observable<Value>;
 };
-export interface ResponseWrapper<Request> {
+export interface RequestWrapper<Request> {
   serviceName: string;
   session: string;
   request: Request;
 }
-export interface ResponseIssued<Request> extends ResponseWrapper<Request> {
+export interface RequestIssued<Request> extends RequestWrapper<Request> {
   status: RequestStatus.Issued;
 }
-export interface ResponseValue<Request, Value> extends ResponseWrapper<Request> {
+export interface ResponseValue<Request, Value> extends RequestWrapper<Request> {
   status: RequestStatus.Value;
   value: Value;
 }
-export interface ResponseCompleted<Request> extends ResponseWrapper<Request> {
+export interface RequestCompleted<Request> extends RequestWrapper<Request> {
   status: RequestStatus.Completed;
 }
-export interface ResponseError<Request, Error> extends ResponseWrapper<Request> {
-  status: RequestStatus.Error;
-  error: Error;
-}
-
 export enum RequestStatus {
   Issued = 'Issued',
   Value = 'Value',
-  Error = 'Error',
   Completed = 'Completed'
 }
-export function build<Request, Value, Error>(
+export function build<Request, Value>(
   serviceName: string,
-  reqHandler: RequestHandler<Request, Value, Error>
+  reqHandler: RequestHandler<Request, Value>
 ) {
   type ResponseItems =
     LocalResponseValue |
-    LocalResponseCompleted |
-    LocalResponseError;
-  type Response = Observable<ResponseItems> & { session: string };
+    LocalResponseCompleted;
+  type ResponseObservable = Observable<ResponseItems>;
   type ServiceNodeItems = LocalResponseIssued | ResponseItems;
-  type ServiceNode = Observable<ServiceNodeItems> & { issueRequest: RequestFn };
-  type LocalResponseIssued = ResponseIssued<Request>;
+  // type ServiceNode = Observable<ServiceNodeItems> & { issueRequest: RequestFn };
+  type LocalResponseIssued = RequestIssued<Request>;
   type LocalResponseValue = ResponseValue<Request, Value>;
-  type LocalResponseCompleted = ResponseCompleted<Request>;
-  type LocalResponseError = ResponseError<Request, Error>;
+  type LocalResponseCompleted = RequestCompleted<Request>;
   interface RequestFn {
-    (request: Request, session?: string): Response;
+    (request: Request, session?: string): ResponseObservable;
   }
   const request$ = new Subject<LocalResponseIssued>();
   const issueRequest: RequestFn = (request: Request, _session?: string) => {
     const session = `${serviceName}:${_session || Math.random()}`;
-    // setTimeout(() => {
     request$.next({
       serviceName,
       session,
       request,
       status: RequestStatus.Issued
     });
-    // }, 0);
     const session$ = value$
       .filter(value => value.session === session);
     const valueResponse$ = session$
       .takeWhile(value => value.status === RequestStatus.Value);
     const lastResponse$ = session$
-      .filter(value => value.status === RequestStatus.Completed || value.status === RequestStatus.Error)
+      .filter(value => value.status === RequestStatus.Completed)
       .take(1);
     const response$ = Observable.merge(valueResponse$, lastResponse$);
     return Object.assign(response$, { session });
-  };
+  }
   const value$ = request$.mergeMap(localRequest => {
     const { session, request } = localRequest;
-    const baseResp = {
-      serviceName,
-      session,
-      request
-    };
-    const responseSubj$ = new Subject<LocalResponseValue | LocalResponseError | LocalResponseCompleted>();
-    const isStillRunning = () => !responseSubj$.closed && !responseSubj$.isStopped;
-    const handlerValue = (value: Value) => {
-      if (isStillRunning()) {
-        responseSubj$.next({
-          ...baseResp,
-          status: RequestStatus.Value,
-          value
-        });
-      }
-    };
-    const handlerError = (error: Error) => {
-      if (isStillRunning()) {
-        responseSubj$.next({
-          ...baseResp,
-          status: RequestStatus.Error,
-          error
-        });
-        responseSubj$.complete();
-      }
-    };
-    const handlerCompleted = () => {
-      if (isStillRunning()) {
-        responseSubj$.next({
-          ...baseResp,
-          status: RequestStatus.Completed
-        });
-        responseSubj$.complete();
-      }
-    };
-    reqHandler(request, handlerValue, handlerError, handlerCompleted);
-    return responseSubj$;
+    // const baseResp = {
+    //   serviceName,
+    //   session,
+    //   request
+    // };
+    return reqHandler(request)
+      .map(value => ({
+        serviceName,
+        session,
+        request,
+        status: RequestStatus.Value,
+        value
+      } as LocalResponseValue))
+      .concat([({
+        serviceName,
+        session,
+        request,
+        status: RequestStatus.Completed
+      } as LocalResponseCompleted)]);
   }).publish();
   value$.connect();
-  const serviceNode$: ServiceNode = Object.assign(
+  const serviceNode$: Observable<LocalResponseValue | LocalResponseCompleted | LocalResponseIssued> = Object.assign(
     Observable.merge(value$, request$),
     {
       issueRequest
